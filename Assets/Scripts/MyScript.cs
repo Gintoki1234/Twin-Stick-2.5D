a73252f925412d197c8d54f1f080b0f3bd002f32
Jubin <jubsi_713@yahoo.com>
2020-09-08 19:57:27 +0100

    Added Some stuffs

---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Added Something");
+        Debug.Log("Added Somethingelse");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
